b63217d7c13e7b3c6a917de1b6abd0e0036087bb
dillthe <seulgi0908@gmail.com>
2023-12-02 20:20:05 -0500

    유저인증까지 파이어베이스로 구현

---
+++ src/components/Nav.svelte
@@ -0,0 +1,74 @@
+<script>
+  export let location;
+  const moveToHome = () => (window.location.hash = "/");
+
+  const moveToMyPage = () => (window.location.hash = "/my");
+</script>
+
+<footer>
+  <div class="footer-block">
+    <button class="footer-icons" on:click={moveToHome}>
+      <div class="footer-icons__img">
+        {#if location === "home"}
+          <img src="assets/home.svg" alt="home" />
+        {:else}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>
+        {/if}
+      </div>
+      <div class="footer-icons__desc">홈</div>
+    </button>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/newspaper.svg" alt="newspaper" />
+      </div>
+      <div class="footer-icons__desc">동네생활</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/location.svg" alt="location" />
+      </div>
+      <div class="footer-icons__desc">내 근처</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/chat.svg" alt="chat" />
+      </div>
+      <div class="footer-icons__desc">채팅</div>
+    </div>
+    <button class="footer-icons" on:click={moveToMyPage}>
+      <div class="footer-icons__img">
+        {#if location === "my"}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z"
+            />
+            <path
+              d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z"
+            />
+          </svg>
+        {:else}
+          <img src="assets/user.svg" alt="" />
+        {/if}
+      </div>
+      <div class="footer-icons__desc">나의 당근</div>
+    </button>
+  </div>
+</footer>
